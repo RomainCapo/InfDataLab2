d34e338c35fb8e148bca20f9d8b20b71d24ebcf0
Romain Capocasale <romain.capocasale99@gmail.com>
2020-10-14 22:09:02 +0200

    add

--- lab2/src/main/java/ch/heigvd/iict/dmg/labo2/Evaluation.java
+++ lab2/src/main/java/ch/heigvd/iict/dmg/labo2/Evaluation.java
@@ -16,87 +16,82 @@ import java.util.function.Function;
 
 public class Evaluation {
 
-    private static Analyzer analyzer = null;
+	private static Analyzer analyzer = null;
 
-    private static void readFile(String filename, Function<String, Void> parseLine)
-            throws IOException {
-        try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(
-                        new FileInputStream(filename),
-                        StandardCharsets.UTF_8)
-        )) {
-            String line = br.readLine();
-            while (line != null) {
-                line = line.trim();
-                if (!line.isEmpty()) {
-                    parseLine.apply(line);
-                }
-                line = br.readLine();
-            }
-        }
-    }
+	private static void readFile(String filename, Function<String, Void> parseLine) throws IOException {
+		try (BufferedReader br = new BufferedReader(
+				new InputStreamReader(new FileInputStream(filename), StandardCharsets.UTF_8))) {
+			String line = br.readLine();
+			while (line != null) {
+				line = line.trim();
+				if (!line.isEmpty()) {
+					parseLine.apply(line);
+				}
+				line = br.readLine();
+			}
+		}
+	}
 
-    /*
-     * Reading CACM queries and creating a list of queries.
-     */
-    private static List<String> readingQueries() throws IOException {
-        final String QUERY_SEPARATOR = "\t";
+	/*
+	 * Reading CACM queries and creating a list of queries.
+	 */
+	private static List<String> readingQueries() throws IOException {
+		final String QUERY_SEPARATOR = "\t";
 
-        List<String> queries = new ArrayList<>();
+		List<String> queries = new ArrayList<>();
 
-        readFile("evaluation/query.txt", line -> {
-            String[] query = line.split(QUERY_SEPARATOR);
-            queries.add(query[1]);
-            return null;
-        });
-        return queries;
-    }
+		readFile("evaluation/query.txt", line -> {
+			String[] query = line.split(QUERY_SEPARATOR);
+			queries.add(query[1]);
+			return null;
+		});
+		return queries;
+	}
 
-    /*
-     * Reading stopwords
-     */
-    private static List<String> readingCommonWords() throws IOException {
-        List<String> commonWords = new ArrayList<>();
+	/*
+	 * Reading stopwords
+	 */
+	private static List<String> readingCommonWords() throws IOException {
+		List<String> commonWords = new ArrayList<>();
 
-        readFile("common_words.txt", line -> {
-            commonWords.add(line);
-            return null;
-        });
-        return commonWords;
-    }
+		readFile("common_words.txt", line -> {
+			commonWords.add(line);
+			return null;
+		});
+		return commonWords;
+	}
 
+	/*
+	 * Reading CACM qrels and creating a map that contains list of relevant
+	 * documents per query.
+	 */
+	private static Map<Integer, List<Integer>> readingQrels() throws IOException {
+		final String QREL_SEPARATOR = ";";
+		final String DOC_SEPARATOR = ",";
 
-    /*
-     * Reading CACM qrels and creating a map that contains list of relevant
-     * documents per query.
-     */
-    private static Map<Integer, List<Integer>> readingQrels() throws IOException {
-        final String QREL_SEPARATOR = ";";
-        final String DOC_SEPARATOR = ",";
+		Map<Integer, List<Integer>> qrels = new HashMap<>();
 
-        Map<Integer, List<Integer>> qrels = new HashMap<>();
+		readFile("evaluation/qrels.txt", line -> {
+			String[] qrel = line.split(QREL_SEPARATOR);
+			int query = Integer.parseInt(qrel[0]);
 
-        readFile("evaluation/qrels.txt", line -> {
-            String[] qrel = line.split(QREL_SEPARATOR);
-            int query = Integer.parseInt(qrel[0]);
+			List<Integer> docs = qrels.get(query);
+			if (docs == null) {
+				docs = new ArrayList<>();
+			}
 
-            List<Integer> docs = qrels.get(query);
-            if (docs == null) {
-                docs = new ArrayList<>();
-            }
+			String[] docsArray = qrel[1].split(DOC_SEPARATOR);
+			for (String doc : docsArray) {
+				docs.add(Integer.parseInt(doc));
+			}
 
-            String[] docsArray = qrel[1].split(DOC_SEPARATOR);
-            for (String doc : docsArray) {
-                docs.add(Integer.parseInt(doc));
-            }
+			qrels.put(query, docs);
+			return null;
+		});
+		return qrels;
+	}
 
-            qrels.put(query, docs);
-            return null;
-        });
-        return qrels;
-    }
-
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
         ///
         /// Reading queries and queries relations files
         ///
@@ -136,6 +131,24 @@ public class Evaluation {
         ///  selected analyzer using performance metrics like F-measure,
         ///  precision, recall,...
         ///
+        int totalRelevantDocs = 0;
+        int totalRetrievedDocs = 0;
+        int totalRetrievedRelevantDocs = 0;
+
+        for(int queryNumber = 1; queryNumber <queries.size();queryNumber++)
+        {
+            List<Integer> queryResults = lab2Index.search(queries.get(queryNumber));
+            List<Integer> qrelResults = qrels.get(queryNumber);
+
+            System.out.println(queryResults);
+            System.out.println(qrelResults);
+            break;
+            
+            //totalRelevantDocs += qrelResults.size();
+            //totalRetrievedDocs += queryResults.size();
+        }
+        
+
 
         // TODO student
         // compute the metrics asked in the instructions
@@ -148,10 +161,6 @@ public class Evaluation {
         //   returned matching a query
         //        List<Integer> qrelResults = qrels.get(queryNumber);
 
-        int queryNumber = 0;
-        int totalRelevantDocs = 0;
-        int totalRetrievedDocs = 0;
-        int totalRetrievedRelevantDocs = 0;
         double avgPrecision = 0.0;
         double avgRPrecision = 0.0;
         double avgRecall = 0.0;
@@ -173,45 +182,42 @@ public class Evaluation {
                 avgPrecisionAtRecallLevels);
     }
 
-    private static void displayMetrics(
-            int totalRetrievedDocs,
-            int totalRelevantDocs,
-            int totalRetrievedRelevantDocs,
-            double avgPrecision,
-            double avgRecall,
-            double fMeasure,
-            double meanAveragePrecision,
-            double avgRPrecision,
-            double[] avgPrecisionAtRecallLevels
-    ) {
-        String analyzerName = analyzer.getClass().getSimpleName();
-        if (analyzer instanceof StopwordAnalyzerBase) {
-            analyzerName += " with set size " + ((StopwordAnalyzerBase) analyzer).getStopwordSet().size();
-        }
-        System.out.println(analyzerName);
-
-        System.out.println("Number of retrieved documents: " + totalRetrievedDocs);
-        System.out.println("Number of relevant documents: " + totalRelevantDocs);
-        System.out.println("Number of relevant documents retrieved: " + totalRetrievedRelevantDocs);
-
-        System.out.println("Average precision: " + avgPrecision);
-        System.out.println("Average recall: " + avgRecall);
-
-        System.out.println("F-measure: " + fMeasure);
-
-        System.out.println("MAP: " + meanAveragePrecision);
-
-        System.out.println("Average R-Precision: " + avgRPrecision);
-
-        System.out.println("Average precision at recall levels: ");
-        for (int i = 0; i < avgPrecisionAtRecallLevels.length; i++) {
-            System.out.println(String.format("\t%s: %s", i, avgPrecisionAtRecallLevels[i]));
-        }
+	private double get_tp_fp_fn_tn(List<Integer>queryResults, List<Integer> qrelResults) {
+		return 0;
+    	
     }
 
-    private static double[] createZeroedRecalls() {
-        double[] recalls = new double[11];
-        Arrays.fill(recalls, 0.0);
-        return recalls;
-    }
+	private static void displayMetrics(int totalRetrievedDocs, int totalRelevantDocs, int totalRetrievedRelevantDocs,
+			double avgPrecision, double avgRecall, double fMeasure, double meanAveragePrecision, double avgRPrecision,
+			double[] avgPrecisionAtRecallLevels) {
+		String analyzerName = analyzer.getClass().getSimpleName();
+		if (analyzer instanceof StopwordAnalyzerBase) {
+			analyzerName += " with set size " + ((StopwordAnalyzerBase) analyzer).getStopwordSet().size();
+		}
+		System.out.println(analyzerName);
+
+		System.out.println("Number of retrieved documents: " + totalRetrievedDocs);
+		System.out.println("Number of relevant documents: " + totalRelevantDocs);
+		System.out.println("Number of relevant documents retrieved: " + totalRetrievedRelevantDocs);
+
+		System.out.println("Average precision: " + avgPrecision);
+		System.out.println("Average recall: " + avgRecall);
+
+		System.out.println("F-measure: " + fMeasure);
+
+		System.out.println("MAP: " + meanAveragePrecision);
+
+		System.out.println("Average R-Precision: " + avgRPrecision);
+
+		System.out.println("Average precision at recall levels: ");
+		for (int i = 0; i < avgPrecisionAtRecallLevels.length; i++) {
+			System.out.println(String.format("\t%s: %s", i, avgPrecisionAtRecallLevels[i]));
+		}
+	}
+
+	private static double[] createZeroedRecalls() {
+		double[] recalls = new double[11];
+		Arrays.fill(recalls, 0.0);
+		return recalls;
+	}
 }
